ab193e60580145cc81b7c666436ad2f091fa62dc
JairoBraga <neto.braga08@gmail.com>
2020-08-16 16:39:04 -0300

    endpoint para retonar os posts de um usuário

--- src/main/java/com/workshop/mongodb/resources/UserResources.java
+++ src/main/java/com/workshop/mongodb/resources/UserResources.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.workshop.mongodb.domain.Post;
 import com.workshop.mongodb.domain.User;
 import com.workshop.mongodb.dto.UserDTO;
 import com.workshop.mongodb.services.UserService;
@@ -37,6 +38,11 @@ public class UserResources {
 	public ResponseEntity<UserDTO> findById(@PathVariable String id){
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
+	}
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 	
 	@DeleteMapping(value = "/{id}")
@@ -60,5 +66,6 @@ public class UserResources {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
+	
 	
 	}
