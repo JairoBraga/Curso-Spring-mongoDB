4c7baa021343c83d33775f81c21bec5b7689b141
JairoBraga <neto.braga08@gmail.com>
2020-08-16 15:55:51 -0300

    Criando entity post com User aninhado

--- src/main/java/com/workshop/mongodb/Instantiation.java
+++ src/main/java/com/workshop/mongodb/Instantiation.java
@@ -1,30 +1,40 @@
 package com.workshop.mongodb;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.workshop.mongodb.domain.Post;
 import com.workshop.mongodb.domain.User;
+import com.workshop.mongodb.repository.PostRepository;
 import com.workshop.mongodb.repository.UserRepository;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
-
+	SimpleDateFormat sdf1 = new SimpleDateFormat("dd/mm/YYY");
+	
 	@Autowired
 	UserRepository userRepository;
+	
+	@Autowired
+	PostRepository postRepository;
 	
 	@Override
 	public void run(String... args) throws Exception {
 		
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
+		Post post1 = new Post(null,sdf1.parse("21/03/2018"),"Partiu viagem","Vou viajar para são paulo!", maria);
+		Post post2 = new Post(null,sdf1.parse("21/03/2018"),"Bom dia","Hoje acordei feliz!", maria);
 		userRepository.saveAll(Arrays.asList(maria,alex,bob));
-		
+		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
 }
